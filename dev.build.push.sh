6d2760c1dc388636ca3b2404ce186cc4c86e93f5
Vic van Gool <vic@cloud66.com>
2025-03-31 08:32:46 +0100

    include main

--- dev.build.push.sh
+++ dev.build.push.sh
@@ -8,4 +8,4 @@ docker buildx create --name multi-builder | true
 # use buildx builder
 docker buildx use multi-builder
 # build and push
-docker buildx build --platform linux/amd64,linux/arm64 -t us-east4-docker.pkg.dev/production-911/docker-private/janitor:$gitref --push .
+docker buildx build --build-arg SHORT_SHA=$gitref --platform linux/amd64,linux/arm64 -t us-east4-docker.pkg.dev/production-911/docker-private/janitor:$gitref --push .
